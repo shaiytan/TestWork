9bb76f3082b0ec57cc2dff6f509f603af7e1f577
shaiytan <postscriptum105@gmail.com>
2018-01-17 19:04:28 +0200

    move sources to separate packages

--- app/src/main/java/shaiytan/ssatrloader/api/GitHubAPI.java
+++ app/src/main/java/shaiytan/ssatrloader/api/GitHubAPI.java
@@ -1,10 +1,11 @@
-package shaiytan.ssatrloader;
+package shaiytan.ssatrloader.api;
 
 import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Url;
+import shaiytan.ssatrloader.models.RepoRecord;
 
 /**
  * Created by Shaiy'tan on 17.01.2018.
---
+++ app/src/main/java/shaiytan/ssatrloader/models/RepoRecord.java
@@ -0,0 +1,40 @@
+package shaiytan.ssatrloader.models;
+
+import java.io.Serializable;
+
+/**
+ * Created by Shaiytan on 17.05.2017.
+ */
+
+public class RepoRecord implements Serializable {
+    private long id;
+    private String name;
+    private String ownerName;
+    private String description;
+    private String avatar;
+
+    public RepoRecord(long id, String name, String ownerName, String description, String avatar) {
+        this.id = id;
+        this.name = name;
+        this.ownerName = ownerName;
+        this.description = description;
+        this.avatar = avatar;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getOwnerName() {
+        return ownerName;
+    }
+    public String getAvatar() {
+        return avatar;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+}
